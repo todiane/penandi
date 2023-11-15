1825584e0fce0c56482ee8e455ece8de52135768
todiane Corri <dcorriette@gmail.com>
2023-11-15 18:38:25 +0000

    Move nav and footer into base.html file and adjust about and blog pages

--- run.py
+++ run.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/blog-home.html")
+def blog():
+    return render_template("blog-home.html")
 
 
 if __name__ == "__main__":
